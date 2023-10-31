a292d126a25ec316ba20796b88d07b7dd80f19f5
Anaïs <aekyon.08@gmail.com>
2023-10-31 15:54:29 +0100

    Finish exercises

--- Navigate/script.js
+++ Navigate/script.js
@@ -1,4 +1,4 @@
-//TODO: Select the last child of the <ol> tag and put it at the beginning of the list
+//Select the last child of the <ol> tag and put it at the beginning of the list
 
 const ol = document.querySelector("ol");
 const olLastChild = ol.children[4];
@@ -6,7 +6,17 @@ const olFirstChild = ol.children[0];
 
 ol.insertBefore(olLastChild, olFirstChild); //CELEBRATE THE SMALL VICTORIEEEES
 
+//Move the <h2> of the third section in the second one and vice-versa
 
-//TODO: Move the <h2> of the third section in the second one and vice-
+const thirdSection = document.querySelectorAll("section")[1];
+const secondSection = document.querySelectorAll("section")[2];
+const thirdsectionH2 = document.querySelectorAll("h2")[1];
+const secondsectionH2 = document.querySelectorAll("h2")[2];
+
+thirdSection.insertBefore(secondsectionH2, thirdSection.firstChild);
+secondSection.insertBefore(thirdsectionH2, secondSection.firstChild);
+
 
 //TODO: Delete the last section from the DOM, we don't need it anyways
+const parent = document.querySelector("main");
+parent.removeChild(secondSection);

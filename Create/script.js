0048cd919b7ed537b990079178fd6a23b8c691ae
Anaïs <aekyon.08@gmail.com>
2023-10-31 12:05:34 +0100

    Add white and black text

--- Create/script.js
+++ Create/script.js
@@ -1,31 +1,15 @@
-/*Modify the script.js to create a new <section> with a random background-color for each learner in your group.
-This section should contain a paragraph with the name of the learner.
-Those sections should be appended in the <article>
-If the background is dark the text should be white, if the background is light the text should be black
-Find a way so that everytime you load the page the order of the elements changes!*/
-
 //add elements
 function addElements() {
 
 //variables
 const learners = ["Lucas", "Kilian", "Alec", "Mohamed", "Pieter", "Rebecca", "Alessandro", "Josué", "Thibault", "Eduarda",
-"Mohammed", "Funda", "Luis", "Alexandru", "Andrej", "Danté", "jonasi", "Kelsey", "Rana", "Jana", "Sieglinde", "Gustave",
+"Mohammed", "Funda", "Luis", "Alexandru", "Andrej", "Danté", "Jonasi", "Kelsey", "Rana", "Jana", "Sieglinde", "Gustave",
 "Sezin", "Thierry", "Sylvie", "Anaïs"]
 const currentArticle = document.querySelector('article');
 
-//create new section for every learner
+//randomize array
 shuffleArray(learners);
 
-learners.forEach(learner => {
-    const newSection = document.createElement("section");
-    const newContent = document.createTextNode(learner);
-    newSection.appendChild(newContent);
-    currentArticle.appendChild(newSection);
-    newSection.style.backgroundColor = getRandomColor();
-  });
-}
-
-//randomize array
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -45,10 +29,27 @@ function getRandomColor() {
   return colors[randomIndex];
 }
 
-// Apply the random color as the background
-//const section = document.querySelector('section');
-//section.style.backgroundColor = getRandomColor();
+//create section for each learner
+learners.forEach(learner => {
+    const newSection = document.createElement("section");
+    const newContent = document.createTextNode(learner);
+    newSection.appendChild(newContent);
+    currentArticle.appendChild(newSection);
 
+//define backgroundColor
+    const backgroundColor = getRandomColor();
+    newSection.style.backgroundColor = backgroundColor;
+
+// white and black text
+  if (backgroundColor === "blue" || backgroundColor === "purple" || backgroundColor === "green"
+  || backgroundColor === "red") {
+    newSection.style.color = "white";
+  } else {
+    newSection.style.color = "black";
+  }
+});
+
+}
 
 // Call the addElements function on page load
 window.onload = addElements;

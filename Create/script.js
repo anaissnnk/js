4e3ff5a8d91d6f45f6239b733b13de4e12ef8147
Anaïs <aekyon.08@gmail.com>
2023-10-31 10:21:16 +0100

    Randomize leaners array

--- Create/script.js
+++ Create/script.js
@@ -4,15 +4,14 @@ Those sections should be appended in the <article>
 If the background is dark the text should be white, if the background is light the text should be black
 Find a way so that everytime you load the page the order of the elements changes!*/
 
-document.body.onload = addElements;
-
 function addElements() {
 
 const learners = ["Lucas", "Kilian", "Alec", "Mohamed", "Pieter", "Rebecca", "Alessandro", "Josué", "Thibault", "Eduarda",
 "Mohammed", "Funda", "Luis", "Alexandru", "Andrej", "Danté", "jonasi", "Kelsey", "Rana", "Jana", "Sieglinde", "Gustave",
 "Sezin", "Thierry", "Sylvie", "Anaïs"]
-
 const currentArticle = document.querySelector('article');
+
+shuffleArray(learners);
 
 learners.forEach(learner => {
     const newSection = document.createElement("section");
@@ -21,3 +20,13 @@ learners.forEach(learner => {
     currentArticle.appendChild(newSection);
   });
 }
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+// Call the addElements function on page load
+document.body.onload = addElements;

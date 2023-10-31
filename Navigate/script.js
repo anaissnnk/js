791f7984a01d1f97c90778916a0454c1bbfc0fa9
Anaïs <aekyon.08@gmail.com>
2023-10-31 14:41:26 +0100

    Switch first and last child

--- Navigate/script.js
+++ Navigate/script.js
@@ -1,2 +1,12 @@
-/*
-*/
+//TODO: Select the last child of the <ol> tag and put it at the beginning of the list
+
+const ol = document.querySelector("ol");
+const olLastChild = ol.children[4];
+const olFirstChild = ol.children[0];
+
+ol.insertBefore(olLastChild, olFirstChild); //CELEBRATE THE SMALL VICTORIEEEES
+
+
+//TODO: Move the <h2> of the third section in the second one and vice-
+
+//TODO: Delete the last section from the DOM, we don't need it anyways

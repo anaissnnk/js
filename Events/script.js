9a1aca9d2e2c74e0a8bc263e05fc554438342fe0
Anaïs <aekyon.08@gmail.com>
2023-10-31 16:59:39 +0100

    Remove comment in Navigate and start Event exercises

--- Events/script.js
+++ Events/script.js
@@ -14,3 +14,23 @@ for (let actionSquare of actionSquares) {
   actionSquare.addEventListener('click', clickOnSquare)
 }
 
+/*my code*/
+
+const actionSquare = document.querySelectorAll(".actionsquare");
+
+function createDiv(){
+  const newDiv = document.createElement('div');
+  newDiv.classList.add("displayedsquare");
+/*TODO: Create a new <div> with a class .displayedsquare and the corresponding clicked color in the div above
+        (.displayedsquare-wrapper)*/
+  newDiv.style.backgroundColor = "red";
+  document.body.appendChild(newDiv);
+};
+
+actionSquares.forEach(function (actionSquare) {
+  actionSquare.addEventListener('click', createDiv);
+});
+
+
+/*TODO: Create a new <li> in the log below to state when the action was done*/
+  

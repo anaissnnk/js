badcafc733a2dd14710d86057aafdf8d990305e6
Anaïs <aekyon.08@gmail.com>
2023-11-02 16:56:00 +0100

    Add 10 collection items

---
+++ Collection/script.js
@@ -0,0 +1,95 @@
+//comfort shows collection
+
+const collection = [
+    //first item
+    {
+        name: 'Little Forest Winter/Spring',
+        type: 'Movie',
+        picture: //picture folder,
+        genre: 'Drama',
+        language: 'Japanese'
+    },
+
+    //second item
+    {
+        name: "Kiki's Delivery Service",
+        type: 'Animation Movie',
+        picture: //picture folder,
+        genre: 'Slice of Life, Drama',
+        language: 'Japanese'
+    },
+
+    //third item
+    {
+        name: "The Makanai",
+        type: 'Series',
+        picture: //picture folder,
+        genre: 'Slice of Life, Drama',
+        language: 'Japanese'
+    },
+
+    //fourth item
+    {
+        name: "Whisper of the Heart",
+        type: 'Animation Movie',
+        picture: //picture folder,
+        genre: 'Slice of Life, Drama',
+        language: 'Japanese'
+    },
+
+    //fifth item
+    {
+        name: "Laid-back Camp",
+        type: 'Anime',
+        picture: //picture folder,
+        genre: 'Slice of Life',
+        language: 'Japanese'
+    },
+
+    //sixth item
+    {
+        name: "Ouran High School Host Club",
+        type: 'Anime',
+        picture: //picture folder,
+        genre: 'Shoujo, Romance, Comedy, Slice of Life',
+        language: 'Japanese'
+    },
+
+    //seventh item
+    {
+        name: "Flying Colours",
+        type: 'Movie',
+        picture: //picture folder,
+        genre: 'Slice of Life, Drama',
+        language: 'Japanese'
+    },
+
+    //heighth item
+    {
+        name: "Howl's Moving Castle",
+        type: 'Animation Movie',
+        picture: //picture folder,
+        genre: 'Romance, Drama',
+        language: 'Japanese'
+    },
+
+    //ninth item
+    {
+        name: "March comes in like a Lion",
+        type: 'Anime',
+        picture: //picture folder,
+        genre: 'Slice of Life, Drama',
+        language: 'Japanese'
+    },
+
+    //tenth item
+    {
+        name: "Boys over Flowers",
+        type: 'Series',
+        picture: //picture folder,
+        genre: 'Romance, Slice of Life, Drama',
+        language: 'Japanese'
+    },
+  
+
+  ]

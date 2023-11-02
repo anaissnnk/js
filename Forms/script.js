a0355a9a834599292833313140e0d05126b8cbda
Anaïs <aekyon.08@gmail.com>
2023-11-02 15:34:30 +0100

    Add the hard truth

--- Forms/script.js
+++ Forms/script.js
@@ -1,7 +1,7 @@
 /* Add a keyup listener on the first input field, so that once you type a letter in this field,
 it goes into the <span id="display-firstname">.The content of the field and the span should always remain the same. */
-const firtinputField = document.querySelector('input');
-const spanfirstName = document.querySelector('span');
+const firtinputField = document.getElementById('firstname');
+const spanfirstName = document.getElementById('display-firstname');
 
 firtinputField.addEventListener("keyup", (event) => {
     if (event.key.match(/[a-zA-Z]/)) { // when typing letters
@@ -14,12 +14,25 @@ firtinputField.addEventListener("keyup", (event) => {
 so that if the age is below 18 the content of the section a-hard-truth remains hidden,
 otherwise show them this hard to swallow fact.*/
 const secondinputField = document.getElementById("age");
+const hardtruthSection = document.getElementById("a-hard-truth");
+
+secondinputField.addEventListener("keyup", () => {
+    if (secondinputField.value > 18) {
+        hardtruthSection.style.visibility = "visible";
+    } else {
+        hardtruthSection.style.visibility = "hidden"; 
+    }
+})
 
 
-
-/*Well this is a common one. Add a keyup listener on both fields and show a visual hint
+/*Add a keyup listener on both fields and show a visual hint
 (for instance turn the field red) if the password is too short (less than 6 characters)
-or if the password and its confirmation do not match.
-Finally, use a change listener on the <select> field to toggle a dark mode on the whole page.
+or if the password and its confirmation do not match.*/
+
+
+
+
+
+/*Finally, use a change listener on the <select> field to toggle a dark mode on the whole page.
 For ease of use, we'll say that the dark mode is just turning the background black and the text white.
 */

971b43d3dfe06d946068e769773fb1773b9fd4ae
Anaïs <aekyon.08@gmail.com>
2023-11-02 11:12:39 +0100

    Create LI with date for each action logged

--- Events/script.js
+++ Events/script.js
@@ -19,28 +19,37 @@ for (let actionSquare of actionSquares) {
 const greenDiv = document.querySelector('.green');
 const violetDiv = document.querySelector('.violet');
 const orangeDiv = document.querySelector('.orange');
-const squareWrapper = document.querySelector('.displayedsquare-wrapper');
+const displayedWrapper = document.querySelector('.displayedsquare-wrapper');
 
 function createDiv(color){
   const newDiv = document.createElement('div');
   newDiv.classList.add("displayedsquare");
   newDiv.style.backgroundColor = color;
-  squareWrapper.append(newDiv);
+  displayedWrapper.append(newDiv);
 };
 
-/*TODO: Create a new <div> with a class .displayedsquare and the corresponding clicked color in the div above
-        (.displayedsquare-wrapper)*/
-
-greenDiv.addEventListener('click', function(){
+greenDiv.addEventListener('click', function() {
   createDiv('lime')
 });
-violetDiv.addEventListener('click', function(){
+violetDiv.addEventListener('click', function() {
   createDiv('magenta')
 });
-orangeDiv.addEventListener('click', function(){
+orangeDiv.addEventListener('click', function() {
   createDiv('orange')
 });
 
-
 /*TODO: Create a new <li> in the log below to state when the action was done*/
-  
+const logSection = document.querySelectorAll('section')[2];
+const squareWrapper = document.querySelector('.actionsquare-wrapper');
+
+function logEntry() {
+  const newEntry = document.createElement('li');
+  const entryTime = new Date();
+  newEntry.textContent = "Action done at " + entryTime.toLocaleTimeString();
+  logSection.append(newEntry);
+}
+
+squareWrapper.addEventListener('click', function() {
+  logEntry()
+}
+)

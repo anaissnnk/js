edb711f932fa00d7f0d1d6c6f9ee71b620bba133
Anaïs <aekyon.08@gmail.com>
2023-11-03 10:55:10 +0100

    Create and append header and header children

--- Collection/script.js
+++ Collection/script.js
@@ -5,7 +5,6 @@ const collection = [
     {
         name: 'Little Forest Winter/Spring',
         type: 'Movie',
-        picture: //picture folder,
         genre: 'Drama',
         language: 'Japanese'
     },
@@ -14,7 +13,6 @@ const collection = [
     {
         name: "Kiki's Delivery Service",
         type: 'Animation Movie',
-        picture: //picture folder,
         genre: 'Slice of Life, Drama',
         language: 'Japanese'
     },
@@ -23,7 +21,6 @@ const collection = [
     {
         name: "The Makanai",
         type: 'Series',
-        picture: //picture folder,
         genre: 'Slice of Life, Drama',
         language: 'Japanese'
     },
@@ -32,7 +29,6 @@ const collection = [
     {
         name: "Whisper of the Heart",
         type: 'Animation Movie',
-        picture: //picture folder,
         genre: 'Slice of Life, Drama',
         language: 'Japanese'
     },
@@ -41,7 +37,6 @@ const collection = [
     {
         name: "Laid-back Camp",
         type: 'Anime',
-        picture: //picture folder,
         genre: 'Slice of Life',
         language: 'Japanese'
     },
@@ -50,7 +45,6 @@ const collection = [
     {
         name: "Ouran High School Host Club",
         type: 'Anime',
-        picture: //picture folder,
         genre: 'Shoujo, Romance, Comedy, Slice of Life',
         language: 'Japanese'
     },
@@ -59,7 +53,6 @@ const collection = [
     {
         name: "Flying Colours",
         type: 'Movie',
-        picture: //picture folder,
         genre: 'Slice of Life, Drama',
         language: 'Japanese'
     },
@@ -68,7 +61,6 @@ const collection = [
     {
         name: "Howl's Moving Castle",
         type: 'Animation Movie',
-        picture: //picture folder,
         genre: 'Romance, Drama',
         language: 'Japanese'
     },
@@ -77,7 +69,6 @@ const collection = [
     {
         name: "March comes in like a Lion",
         type: 'Anime',
-        picture: //picture folder,
         genre: 'Slice of Life, Drama',
         language: 'Japanese'
     },
@@ -86,10 +77,37 @@ const collection = [
     {
         name: "Boys over Flowers",
         type: 'Series',
-        picture: //picture folder,
         genre: 'Romance, Slice of Life, Drama',
         language: 'Japanese'
     },
   
 
   ]
+
+//header
+const header = document.createElement("header");
+document.body.append(header);
+
+const headerImage = document.createElement("img");
+headerImage.src = 'https://i.pinimg.com/564x/15/9b/7b/159b7bc11901446c7463e4ce540da53e.jpg';
+header.append(headerImage);
+
+const headerParagraph = document.createElement("p");
+headerParagraph.textContent = "Ananaïs' comfort shows for a cozy time under the blanket sipping hot tea and eating cakes. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+header.append(headerParagraph);
+
+//body
+const main = document.createElement("main");
+document.body.append(main);
+
+const rowDiv = document.createElement("div");
+rowDiv.setAttribute("class", "row");
+main.append(rowDiv);
+
+const card = document.createCard
+
+
+collection.forEach(movie) => {
+//blablabla
+    
+};

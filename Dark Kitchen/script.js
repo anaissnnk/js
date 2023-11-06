b578f3547d51ae51b0861b2bc346ec6a611bff5f
Anaïs <aekyon.08@gmail.com>
2023-11-06 16:18:34 +0100

    Change filter name, change 2 last cards contents in js

--- Dark Kitchen/script.js
+++ Dark Kitchen/script.js
@@ -10,7 +10,7 @@ const products = [
 
     //SECOND PRODUCT
     {
-        name: "Matcha Madeleines",
+        name: "Soft Honeyed Matcha Madeleines",
         tags: "Dessert",
         description: "blablabla",
         price: "4 euros",
@@ -18,7 +18,7 @@ const products = [
 
     //THIRD PRODUCT
     {
-        name: "Chocolate Brownie",
+        name: "Fudgy Almond Chocolate Brownie",
         tags: "Dessert",
         description: "blablabla",
         price: "5 euros",
@@ -65,6 +65,45 @@ const products = [
     },
 
 ]
+
+//FILTER - W3SCHOOL
+// JavaScript code in "script.js"
+function filterSelection(c) {
+    var x, i;
+    x = document.getElementsByClassName("filterDiv");
+  
+    // Loop through the elements with the "filterDiv" class
+    for (i = 0; i < x.length; i++) {
+      // If the button's category matches the element's class or it's "all," show the element
+      if (c == "all" || x[i].classList.contains(c)) {
+        x[i].style.display = "block";
+      } else {
+        // Otherwise, hide the element
+        x[i].style.display = "none";
+      }
+    }
+  }
+  
+  // Function to add the "active" class to the clicked button
+  function setActive(btn) {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    btn.className += " active";
+  }
+  
+  // Add event listeners to the filter buttons
+  var btns = document.getElementsByClassName("btn");
+  for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+      var category = this.getAttribute("data-category");
+      filterSelection(category);
+      setActive(this);
+    });
+  }
+  
+  // Initial call to show all items
+  filterSelection("all");
+
 
 //FUNCTIONS
 //Product Name

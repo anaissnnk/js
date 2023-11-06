ac32e56f6bf3fec0226c987b0128365a4ca72292
Anaïs <aekyon.08@gmail.com>
2023-11-06 10:58:23 +0100

    Create const products in js

---
+++ Dark Kitchen/script.js
@@ -0,0 +1,67 @@
+// PRODUCTS COLLECTION
+const products = [
+    //FIRST PRODUCT
+    {
+        name: "Fudgy Matcha Brownie",
+        tags: "Dessert",
+        description: "blablabla",
+        price: "5 euros",
+    },
+
+    //SECOND PRODUCT
+    {
+        name: "Dark Chocolate Brownie",
+        tags: "Dessert",
+        description: "blablabla",
+        price: "4 euros",
+    },
+
+    //THIRD PRODUCT
+    {
+        name: "Matcha White Chocolate Cookie",
+        tags: "Dessert",
+        description: "blablabla",
+        price: "5 euros",
+    },
+
+    //FOURTH ITEM 
+    {
+        name: "Bulbasaur Cookies",
+        tags: "Dessert",
+        description: "blablabla",
+        price: "3 euros",
+    },
+
+    //FIFTH ITEM
+    {
+        name: "Baked Matcha Basque Cheesecake",
+        tags: "Dessert",
+        description: "blablabla",
+        price: "7 euros",
+    },
+
+    //SIXH ITEM
+    {
+        name: "Baked Speculoos and Chocolate Cheesecake",
+        tags: "Dessert",
+        description: "blablabla",
+        price: "7 euros",
+    },
+
+    //SEVENTH ITEM
+    {
+        name: "Taro Milk Tea",
+        tags: "Drink",
+        description: "blablabla",
+        price: "5 euros",
+    },
+
+    //EIGHTH ITEM
+    {
+        name: "Brown Sugar Milk Tea",
+        tags: "Drink",
+        description: "blablabla",
+        price: "5 euros",
+    },
+
+]

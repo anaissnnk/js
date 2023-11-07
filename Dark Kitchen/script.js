feb55d91d9a625a9d97b0bb04fc4550307e6ce96
Anaïs <aekyon.08@gmail.com>
2023-11-07 11:53:11 +0100

    Add funcion to click on icon cart and display cart tab

--- Dark Kitchen/script.js
+++ Dark Kitchen/script.js
@@ -127,5 +127,11 @@ document.body.querySelectorAll("p.product-price").forEach((p, index) => {
 });
 
 //shopping cart
+let iconCart = document.querySelector('.icon-cart');
+let body = document.querySelector("body");
+
+iconCart.addEventListener('click', () => {
+  body.classList.toggle('showCart');
+})
 
 
